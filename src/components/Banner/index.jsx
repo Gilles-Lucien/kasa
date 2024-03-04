7dc3cef239bba78e3c75fec28389a297b52f8439
Gilles Lucien <gpeltier2@gmail.com>
2024-03-04 11:42:20 +0100

    Refactor Banner and Navbar styles

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,12 +1,16 @@
-import BannerImg from "../../assets/Banner.png";
+// import BannerImg from "../../assets/Banner.png";
 import "./Banner.css";
 
 function Banner() {
-return (
+  return (
+    // <div className="Banner">
+    //     <h1>Chez vous, partout et ailleurs</h1>
+    // </div>
     <div className="Banner">
         <h1>Chez vous, partout et ailleurs</h1>
+        <div className="Banner__img"></div>
     </div>
-);
+  );
 }
 
 export default Banner;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,38 +1,20 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo from "../../assets/LOGO.png";
 import "./Navbar.css";
-import { useEffect } from "react";
-
-// Notes : utiliser le hook useNavLink pour gérer l'effet d'underline
-// voir la doc react router dom 
-
 
 function Navbar() {
-  // Add an event listener to each link in the header to underline the active link
-  useEffect(() => {
-    const links = document.querySelectorAll(".header__link");
-    links.forEach((link) => {
-      link.addEventListener("click", () => {
-        links.forEach((link) => {
-          link.classList.remove("header__link__underlined");
-        });
-        link.classList.add("header__link__underlined");
-      });
-    });
-  }, []);
-
   return (
     <header className="header">
-      <Link className="logo" to="/">
+      <NavLink className="logo" to="/">
         <img src={Logo} alt="Kasa_Logo" />
-      </Link>
+      </NavLink>
       <div className="header__links">
-        <Link className="header__link" to="/">
+        <NavLink className="header__link" to="/">
           Accueil
-        </Link>
-        <Link className="header__link" to="/about">
+        </NavLink>
+        <NavLink className="header__link" to="/about">
           À propos
-        </Link>
+        </NavLink>
       </div>
     </header>
   );
