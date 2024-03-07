a0e180a8bf4609b7735c9ac7e5bde51bf2e5a074
Gilles Lucien <gpeltier2@gmail.com>
2024-03-07 18:45:00 +0100

    Refactor collapse component styles and add collapse functionality

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import "./Collapse.css";
 import arrowCollapse from "../../assets/arrowCollapse.svg";
 
 function Collapse(props) {
   console.log("props :", props);
 
+function handleCollapseClick(e) {
+    const collapse = e.target.closest(".collapse").querySelector(".collapse__content");
+    collapse.classList.toggle("show");
+    e.target.querySelector("img").classList.toggle("rotate");
+  }
+
+
   return (
     <>
       {Object.keys(props).map((key) => (
         <div className="collapse" key={key}>
-          <div className="collapse__title">
+          <div className="collapse__title" onClick={handleCollapseClick}>
             <img src={arrowCollapse} alt="arrowCollapse" />
             <h2>{key}</h2>
           </div>
           {Array.isArray(props[key]) ? (
-            <div className="collapse__content show">
+            <div className="collapse__content">
               <ul>
                 {props[key].map((item, index) => (
                   <li key={index}>{item}</li>
@@ -23,7 +30,7 @@ function Collapse(props) {
               </ul>
             </div> 
           ) : (
-            <div className="collapse__content show">
+            <div className="collapse__content">
               <p>{props[key]}</p>
             </div>
           )}
